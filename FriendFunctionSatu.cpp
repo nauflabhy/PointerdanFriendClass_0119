3fffe35007dd8469113a65f455f09bbbde80ee20
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-06 19:15:26 +0700

    menambahkan fungsi objek

--- FriendFunctionSatu.cpp
+++ FriendFunctionSatu.cpp
@@ -8,3 +8,14 @@ class mahasiswa {
     public:
         friend void setNama (mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b) {
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main (){
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
